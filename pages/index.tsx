c1f5d22d51fe4bc111cc094c1d28a6abc4168a8e
Sarah Bateman <sarahb3865@gmail.com>
2023-09-16 20:06:19 +1200

    Changed title to test github actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App Test 1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
