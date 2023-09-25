c2e116c52018601819c11e1ca28d74c28109bf3c
Sarah Bateman <sarahb3865@gmail.com>
2023-09-25 15:21:58 +1300

    Added basic testing POST functionality on user page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,68 @@
+import { GetServerSideProps } from 'next';
+import SortableTable from "../pages/components/table/SortableTable";
 import Head from 'next/head';
 import styles from '@/pages/index.module.css';
 import Link from 'next/link';
+import { NextPage } from 'next';
+import React, { useState } from "react";
 
-export default function Home() {
+interface ArticlesInterface {
+  id: string;
+  dateSubmitted: String,
+  articleTitle: String,
+  articleCitation: String,
+  summary: String;
+}
+
+type ArticlesProps = {
+  articles: ArticlesInterface[];
+};
+
+const Articles: NextPage<ArticlesProps> = ({ articles }) => {
+  const headers: { key: keyof ArticlesInterface; label: string }[] = [
+    { key: "dateSubmitted", label: "Title" },
+    { key: "articleTitle", label: "Authors" },
+    { key: "articleCitation", label: "Source" },
+    { key: "summary", label: "Summary" },
+  ];
+
+  const [articleTitle, setTitle] = useState("");
+  const dateSubmitted = "placeholder";
+  const articleCitation = "placeholder";
+  const summary = "placeholder";
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://speed-back-end-git-feature-working-cise5001.vercel.app/api/articles', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({dateSubmitted, articleTitle, articleCitation, summary}),
+      });
+
+      if (response.ok) {
+        console.log('Submission successful');
+        // Reset the title state if needed
+        setTitle('');
+      } else {
+        console.log('Submission failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('There was an error submitting the form:', error);
+    }
+    console.log(
+      JSON.stringify({
+        articleTitle, 
+        dateSubmitted, 
+        articleCitation,
+        summary
+      })
+    );
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,6 +78,20 @@ export default function Home() {
         <Link href="/analyst">
           <button>Analyst</button>
         </Link>
+        <h2>Submit an Article for Moderation</h2>
+        <form id="userSubmit" onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          name="articleTitle" 
+          placeholder="Enter article title here" 
+          value={articleTitle}
+          onChange={event => setTitle(event.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+          <SortableTable headers={headers} data={articles} />
+        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -25,4 +99,49 @@ export default function Home() {
       </footer>
     </div>
   );
+};
+
+const getTopics = async() => {
+  try {
+      const res = await fetch('http://speed-back-end-git-feature-working-cise5001.vercel.app/api/articles', {
+          cache: 'no-store',
+      });
+
+      if(!res.ok) {
+          throw new Error("Failed to fetch topics")
+      }
+
+      return res.json();
+  }
+  catch (error) {
+      console.log("Error loading topics: ", error);
+  }
 }
+
+export const getServerSideProps: GetServerSideProps<ArticlesProps> = async (_) => {
+
+  const { topics } = await getTopics();
+  console.log(topics, "Testing String");
+
+  // Map the data to ensure all articles have consistent property names
+  const articles = topics.map((article: { 
+    id: any; _id: any; title: any; authors: any; source: any; pubyear: any; doi: any; claim: any; evidence: any; }) => ({
+    id: article.id ?? article._id,
+    title: article.title,
+    authors: article.authors,
+    source: article.source,
+    pubyear: article.pubyear,
+    doi: article.doi,
+    claim: article.claim,
+    evidence: article.evidence,
+  }));
+
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
+
+export default Articles; 
