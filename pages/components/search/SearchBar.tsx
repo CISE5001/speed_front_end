0f755613f0eed72f32f72ae0807b6414e5eab4b5
Kylie Crump <moarsomecoarsome@gmail.com>
2023-10-01 22:20:41 +1300

    Added search bar to homepage

---
+++ pages/components/search/SearchBar.tsx
@@ -0,0 +1,42 @@
+// SearchBar.tsx
+import React, { useState, ChangeEvent } from 'react';
+
+interface SearchBarProps {
+  onSearch: (data: any) => void;
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [query, setQuery] = useState<string>('');
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`https://speed-back-end-git-feature-working-cise5001.vercel.app/api/articles`);
+      if (response.ok) {
+        const data = await response.json();
+        onSearch(data.topics.filter((topic: { articleTitle: string; }) => topic.articleTitle.includes(query))); // Pass the search results to a callback
+      } else {
+        console.error('Error searching:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error searching:', error);
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
