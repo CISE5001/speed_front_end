d0a5fa22f903c1db133219f5cdd0099fb066a963
Sarah Bateman <sarahb3865@gmail.com>
2023-10-12 13:14:15 +1300

    Fixed issue with admin delete still submitting to Home.

--- pages/editArticle/[id]/index.tsx
+++ pages/editArticle/[id]/index.tsx
@@ -146,11 +146,12 @@ export default function Home({ submittedArticles }: HomeProps) {
           </div>
 
           <div className="mt-6">
-            <button
-              onClick={handleDelete}
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-              Delete
-            </button>
+          <button
+            type="button"
+            onClick={handleDelete}
+            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
+            Delete
+          </button>
           </div>
         </form>
       </main>
